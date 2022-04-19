febe853326d4a0d60fa63175edab3403d525d69a
Eduardo Canellas <eduardocanellas98@gmail.com>
2022-04-18 21:51:56 -0300

    add construtores das entidades

--- src/main/java/org/example/domain/entities/Boleto.java
+++ src/main/java/org/example/domain/entities/Boleto.java
@@ -1,10 +1,12 @@
 package org.example.domain.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
 public class Boleto {
     private double valorPago;
     private LocalDateTime data;
--- src/main/java/org/example/domain/entities/Fatura.java
+++ src/main/java/org/example/domain/entities/Fatura.java
@@ -1,10 +1,12 @@
 package org.example.domain.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
 public class Fatura {
     private String nomeCliente;
     private LocalDateTime data;
